a86e2d77b1e2520266e0808deb16c83eb8b8b53a
Rachel Frederickson <vampirerachel@gmail.com>
2023-07-05 00:56:14 -0400

    Removed persistor from code

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { store, persistor } from "./redux/store";
+import { store} from "./redux/store";
 import App from "./components/App";
 
 const rootElement = document.getElementById("root");
 
-// Update the import and usage of createRoot
 createRoot(rootElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
         <App />
-      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,36 +1,20 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-import { persistStore, persistReducer } from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
-
-import contactsReducer, {
-  fetchContacts,
-} from "./contactReducer";
+import contactsReducer, { fetchContacts } from "./contactReducer";
 import filterReducer from "./filterReducer";
-
-const persistConfig = {
-  key: "root",
-  storage,
-  version: 1,
-};
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
   filter: filterReducer,
 });
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
   devTools: process.env.NODE_ENV === "development",
 });
-
-export const persistor = persistStore(store);
 
 store.dispatch(fetchContacts());
 
